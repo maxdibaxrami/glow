cab80bc2f160e651d9ff5f1e5da90405e02a149a
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-10-14 18:43:45 +0300

    m

--- src/components/swap/index.jsx
+++ src/components/swap/index.jsx
@@ -1,6 +1,9 @@
 import react from 'react'
 import { EffectCards } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Button} from '@geist-ui/react'
+import { HeartFill, X } from '@geist-ui/react-icons'
+
 import './style.css'
 import 'swiper/css';
 import 'swiper/css/effect-cards';
@@ -13,7 +16,14 @@ const SwapperC = () => {
         modules={[EffectCards]}
         className="mySwiper"
     >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>
+            
+            <div className='swiper-slide--button---div '>
+                 <Button className='swiper-slide--button' iconRight={<X size={52}/>} auto scale={3/3} px={1} />
+                 <Button className='swiper-slide--button' iconRight={<HeartFill size={52} />} auto scale={3/3} px={1} />
+            </div>
+
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
